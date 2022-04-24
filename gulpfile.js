0827098dd9cad29ecad310eac9c2bd5ab2c3e337
Enisz Abdalla <enisz87@gmail.com>
2022-04-24 18:58:03 +0200

    #5 updated gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,28 @@ function locales() {
     return copy("locale", "dist/locale");
 }
 
+function major() {
+    return spawn("npm", ["version", "major"], {stdio: "inherit" });
+}
+
+function minor() {
+    return spawn("npm", ["version", "minor"], {stdio: "inherit" });
+}
+
+function patch() {
+    return spawn("npm", ["version", "patch"], {stdio: "inherit" });
+}
+
+function publish() {
+    return spawn("npm", ["publish"], {stdio: "inherit" });
+}
+
 exports.default = () => {
     return spawn("gulp", ["--tasks"], { stdio: "inherit" });
 };
 
 exports.build = series(build, locales);
+exports.major = major;
+exports.minor = minor;
+exports.patch = patch;
+exports.publish = publish;
